b02f6e143cbe4e657beba0768e9a26c26dd656e7
Mac Maxwell <macsakini@hotmail.com>
2022-04-27 16:40:25 +0300

    Create algo.java

---
+++ algo.java
@@ -0,0 +1,47 @@
+class algo {
+    public static void main(String[] args) {
+        int[] f ={4,2,-3,5,8,12,1,0};
+        algo(f,0,7); 
+    }
+    public static int[] algo(int[] f, int l, int r) {
+    
+        int x = 0;
+        int y = 0;
+        
+        if(l == r){
+            x = f[l];
+            y =f[l];
+        } else if(l+1 == r){
+            if(f[l]<f[r]){
+                x =f[r];
+                y = f[l];
+            }
+            else{
+                x =f[l];
+                y = f[r];
+            }
+        }else{
+            int q =l+(r-1)/2;
+            
+            int[] xlyl = algo(f,l,q);
+            int[] xryr = algo(f,q+1,r);
+        }
+
+        if(xlyl[0] > xryr[0]){
+            x = xlyl[0];
+        }else{
+            x = xryr[0];
+        }
+
+        if(xlyl[0] < xryr[0]){
+            y = xlyl[0];
+        }else{
+            y = xryr[0];
+        }
+        int[] ret = {x,y};
+
+        System.out.println(x,y);
+
+        return ret;
+    }    
+}
