17e09b74057bfd97595756966537b7aea9ca577a
Mac Maxwell <macsakini@hotmail.com>
2022-04-28 14:02:06 +0300

    Trees

--- algo.java
+++ algo.java
@@ -11,6 +11,10 @@ class algo {
         if(l == r){
             x = f[l];
             y =f[l];
+
+            int[] ret = {x,y};
+            
+            return ret;
         } else if(l+1 == r){
             if(f[l]<f[r]){
                 x =f[r];
@@ -20,28 +24,33 @@ class algo {
                 x =f[l];
                 y = f[r];
             }
+
+            int[] ret = {x,y};
+
+            return ret;
         }else{
-            int q =l+(r-1)/2;
+            int q = (l+(r-1))/2;
             
             int[] xlyl = algo(f,l,q);
+
             int[] xryr = algo(f,q+1,r);
+
+            if(xlyl[0] > xryr[0]){
+                x = xlyl[0];
+            }else{
+                x = xryr[0];
+            }
+
+            if(xlyl[0] < xryr[0]){
+                y = xlyl[0];
+            }else{
+                y = xryr[0];
+            }
+
+            System.out.println(x);
+
+            int[] ret = {x,y};
+            return ret;
         }
-
-        if(xlyl[0] > xryr[0]){
-            x = xlyl[0];
-        }else{
-            x = xryr[0];
-        }
-
-        if(xlyl[0] < xryr[0]){
-            y = xlyl[0];
-        }else{
-            y = xryr[0];
-        }
-        int[] ret = {x,y};
-
-        System.out.println(x,y);
-
-        return ret;
     }    
 }
